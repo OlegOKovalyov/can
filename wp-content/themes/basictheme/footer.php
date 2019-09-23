db227b1793acd6e6edeb4d71234c490c4b3853f0
Oleg Kovalyov <koa2003@ukr.net>
2019-09-23 18:29:47 +0300

    DT-56 Evening commit.

---
+++ wp-content/themes/basictheme/footer.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * The template for displaying the footer
+ */
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+            <h5 class="text-center">+31 6 00 00 00 00 . Address . Zipcode . City</h5>
+            <p class="text-center mb-4">Copyright © CAN. All rights reserved.</p>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+<!--<script-->
+<!--        src="https://code.jquery.com/jquery-3.4.1.min.js"-->
+<!--        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="-->
+<!--        crossorigin="anonymous"></script>-->
+<!--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+<script>
+    jQuery(document).ready(function() {
+        jQuery('a[href^="#section"]').click(function() {
+            var target = jQuery(this).attr('href');
+            jQuery('html, body').animate({
+                scrollTop: jQuery(target).offset().top-174
+            }, 1500);
+        }); // click()
+        $(window).scroll(function() {
+            if ($(document).scrollTop() > 10) {
+                $("#masthead").css('background-color','#6d948c')
+                    .css('transition', '2s');
+                $(".hero_video .wpb_wrapper.vc_figure .vc_single_image-wrapper img")
+                    .animate({width:'350',height:'104',left:'43',top:'-438',opacity:0,zIndex:'3'}, 10);
+                $(".site-branding .custom-logo-link img").css('display', 'block')
+                    .css('transition', '2s');
+            } else {
+                $("#masthead").css('background-color', 'transparent')
+                    .css('transition', '1s');
+                $(".site-branding .custom-logo-link img").css('display', 'none')
+                    .css('transition', '2s');
+                $(".hero_video .wpb_wrapper.vc_figure .vc_single_image-wrapper img")
+                    .css('transition', '2s');
+            }
+        }); // scroll()
+    }); // ready()
+</script>
+
+</body>
+</html>
---
+++ wp-content/themes/basictheme/header.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The header for our theme
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+	<header id="masthead" class="site-header">
+        <div class="logo">
+            <div class="site-branding">
+                <?php the_custom_logo(); ?>
+            </div><!-- .site-branding -->
+        </div>
+        <!-- Navigation -->
+        <nav id="site-navigation" class="main-navigation navbar navbar-expand-lg navbar-light primary-menu w-100" style="background-color: #b1cbd8;">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <?php wp_nav_menu( array(
+                    'theme_location' => 'menu-1',
+                    'menu_id'        => 'primary-menu',
+                ) );
+                ?>
+            </div>
+        </nav>
+
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
